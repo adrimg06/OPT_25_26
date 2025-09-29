f8d407c0c44e01798c13b8da65a3111792e039fe
AdrianMG <moralesadrian422@gmail.com>
2025-09-29 13:37:08 +0200

    .

---
+++ Unidad_1/b1_2_calculadora_simple.py
@@ -0,0 +1,16 @@
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+modulo = num1 % num2
+potencia = num1 ** num2
+
+print(f"Suma: {suma}")
+print(f"Resta: {resta}")
+print(f"Multiplicación: {multiplicacion}")
+print(f"División: {division}")
+print(f"Módulo: {modulo}")
+print(f"Potencia: {potencia}")
